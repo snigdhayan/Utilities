5f5eb328dd2206263af9b74ce0a6bdb0ad4bacf1
Snigdhayan Mahanta <snigdhayan@gmail.com>
2021-08-09 09:18:09 +0200

    Minor changes

--- Anaconda/launchAnaconda.sh
+++ Anaconda/launchAnaconda.sh
@@ -2,6 +2,6 @@
 
 docker build -f ./Dockerfile.txt -t myanaconda .
 
-docker run -t -d -v $(pwd)/myFolder:/myFolder -p 8888:8888 --name myanaconda myanaconda
+docker run -t -d -v $(pwd)/MyAnaconda:/MyAnaconda -p 8888:8888 --name myanaconda myanaconda
 
 docker exec -it myanaconda jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --port=8888
