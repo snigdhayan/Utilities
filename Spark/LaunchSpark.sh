347a975e8648a402cbc64005c3d80f5d644caffb
Snigdhayan Mahanta <snigdhayan@gmail.com>
2021-08-12 12:56:14 +0200

    Minor change

--- Spark/LaunchSpark.sh
+++ Spark/LaunchSpark.sh
@@ -7,3 +7,5 @@ docker build -f ./Dockerfile.txt -t myspark .
 docker run -d -v $(pwd)/MySpark:/MySpark -p 8888:8888 -e ACCEPT_EULA=yes myspark
 
 printf "Navigate to localhost:8888 to open Jupyter Notebook.\n"
+
+# docker system prune -a -f
