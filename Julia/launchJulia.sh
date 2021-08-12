69b0dce061ae2c413f16db5fe4121cd9a1d12b25
Snigdhayan Mahanta <snigdhayan@gmail.com>
2021-08-12 14:23:01 +0200

    Minor change

--- Julia/launchJulia.sh
+++ Julia/launchJulia.sh
@@ -4,6 +4,4 @@ docker build -f ./Dockerfile.txt -t myjulia .
 
 docker run -t -d -p 8888:8888 --name myjulia myjulia
 
-# docker exec -it myludwig jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --port=8888
-
 # docker system prune -a -f
